316773af34856029e2e93897de290ac983fee6a4
james <james@mcmurran.org>
2018-11-22 18:52:00 -0500

    Removed some old html

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once 'DB.php';
 
 $DB= new \budget\DB();
 $txns=$DB->listTransactions();
-var_dump($txns);
 
 ?>
 <html>
@@ -23,14 +22,6 @@ var_dump($txns);
         </tr>
         </thead>
         <tbody>
-            <tr>
-                <td style="width: 148px;">Monthly Remaining</td>
-                <td style="width: 182px;">=1900*2</td>
-            </tr>
-            <tr>
-                <td style="width: 148px;">Bills Paid</td>
-                <td style="width: 182px;">Select sum(amount)</td>
-            </tr>
             <tr>
                 <td style="width: 148px;">Bills remaining</td>
                 <td style="width: 182px;">&nbsp;</td>
