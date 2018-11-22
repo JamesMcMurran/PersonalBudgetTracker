e7aa5675fb19da70709f97826bed7fe4dc18a00f
james <james@mcmurran.org>
2018-11-22 18:01:42 -0500

    Forgot the pross file

---
+++ pross.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'DB.php';
+
+//check Auth
+
+//Check post
+
+if(!empty($_POST['category'])){
+	$DB= new \budget\DB();
+
+	$type = $_POST['type'];
+	$category = $_POST['category'];
+	$name = $_POST['name'];
+	$amount = $_POST['amount'];
+
+	$DB->insertMovie($type,$category,$name,$amount);
+}else{
+    echo 'No data sent';
+}
