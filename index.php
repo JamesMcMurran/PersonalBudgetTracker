ad2e593b13df26258882efc83d071a49598ef185
JamesMcMurran <james@mcmurran.org>
2018-11-22 17:08:27 -0500

    Create index.php
    First ruff of UI

---
+++ index.php
@@ -0,0 +1,69 @@
+<h1 style="color: #5e9ca0;">Budget Tracking</h1>
+<p>&nbsp;</p>
+<h2 style="color: #2e6c80;">Quick look:</h2>
+<p>Month: November</p>
+<table class="editorDemoTable" style="width: 342px;">
+<thead>
+<tr>
+<td style="width: 148px;">&nbsp;</td>
+<td style="width: 182px;">Example</td>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="width: 148px;">Monthly Remaining</td>
+<td style="width: 182px;">=1900*2</td>
+</tr>
+<tr>
+<td style="width: 148px;">Bills Paid</td>
+<td style="width: 182px;">Select sum(amount)</td>
+</tr>
+<tr>
+<td style="width: 148px;">Bills remaining</td>
+<td style="width: 182px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 148px;">Monthly Food</td>
+<td style="width: 182px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 148px;">Food Budget</td>
+<td style="width: 182px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 148px;">&nbsp;</td>
+<td style="width: 182px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 148px;">Other</td>
+<td style="width: 182px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 148px;">&nbsp;</td>
+<td style="width: 182px;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 148px;">&nbsp;</td>
+<td style="width: 182px;">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><strong>&nbsp;Add Item</strong></p>
+<form><select>
+<option value="Bill">Bill</option>
+<option value="Food">Food</option>
+<option value="Other">Other</option>
+</select>
+<p>&nbsp;</p>
+</form><form>
+<p><br />Category<br /> <input id="Catagory" type="text" /></p>
+</form><form>
+<p>Name<br /> <input id="Catagory" type="text" /></p>
+<p>&nbsp;</p>
+</form>
+<p><strong>Bill&nbsp;</strong><strong>- Gas Electric Car<br /></strong><strong>Food </strong><strong>- Groshires Restaurant&nbsp;</strong></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
