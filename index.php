8c99ee0353163a7efd64245fdc584d64f8705870
james <james@mcmurran.org>
2018-11-24 21:41:12 -0500

    missed a "

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!isset($_SESSION['user'])){
-    echo '<a href="http://smartbudgettracker.com/simpleLogin.php>Login</a>"';
+    echo '<a href="http://smartbudgettracker.com/simpleLogin.php">Login</a>';
     die();
 }
 
